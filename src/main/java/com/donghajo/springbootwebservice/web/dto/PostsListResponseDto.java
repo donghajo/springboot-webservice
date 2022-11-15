69db36f4e26a4531130d6473d641dbe2e67703a7
jodongha <jodongrami@gmail.com>
2022-11-16 02:16:17 +0900

    implement D

---
+++ src/main/java/com/donghajo/springbootwebservice/web/dto/PostsListResponseDto.java
@@ -0,0 +1,21 @@
+package com.donghajo.springbootwebservice.web.dto;
+
+import com.donghajo.springbootwebservice.web.domain.posts.Posts;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+public class PostsListResponseDto {
+    private Long id;
+    private String title;
+    private String author;
+    private LocalDateTime modifiedDate;
+
+    public PostsListResponseDto(Posts posts){
+        this.id = posts.getId();
+        this.title = posts.getTitle();
+        this.author = posts.getAuthor();
+        this.modifiedDate = posts.getModifiedDate();
+    }
+}
