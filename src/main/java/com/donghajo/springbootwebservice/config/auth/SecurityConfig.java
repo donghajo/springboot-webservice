4e95501141a7818f4218a0b06403453afb9e544a
jodongha <jodongrami@gmail.com>
2022-11-23 01:27:52 +0900

    add OAuth2

---
+++ src/main/java/com/donghajo/springbootwebservice/config/auth/SecurityConfig.java
@@ -0,0 +1,40 @@
+package com.donghajo.springbootwebservice.config.auth;
+
+import com.donghajo.springbootwebservice.web.domain.user.Role;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+
+@Configuration
+@RequiredArgsConstructor
+@EnableWebSecurity // Spring Security 설정 활성화
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final CustomOAuth2UserService customOAuth2UserService;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http
+                .csrf().disable()
+                .headers().frameOptions().disable()
+                .and()
+                .authorizeRequests()
+                .antMatchers("/", "/css/**", "/images/**",
+                        "/js/**").permitAll()
+                .antMatchers("/api/v1/**").hasRole(Role.
+                        USER.name())
+                .anyRequest().authenticated()
+                .and()
+                .logout()
+                .logoutSuccessUrl("/")
+                .and()
+                .oauth2Login()
+                .userInfoEndpoint()
+                .userService(customOAuth2UserService);
+    }
+
+}
